f4286e7bce8ef3dd92103ace3a23fb2ea304afa1
vincentkumalo <142814100+vincentkumalo@users.noreply.github.com>
2024-11-04 17:49:50 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_9kqq60X4L', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7djm1sbofl804gc5ruqua1s4qu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
